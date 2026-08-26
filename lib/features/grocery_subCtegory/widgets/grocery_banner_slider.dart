057916c628ffc9658a97717d707ada209305588c
sneha <sneha.jadhav@neosao.co.in>
2026-08-26 10:38:45 +0530

    banner slider added

---
+++ lib/features/grocery_subCtegory/widgets/grocery_banner_slider.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../core/utils/responsive_utils.dart';
+import '../../../routes/app_route_path.dart';
+import '../data/models/homePage_model.dart';
+
+class GroceryBannerSlider extends StatefulWidget {
+  final List<BannerModel> banners;
+
+  const GroceryBannerSlider({
+    super.key,
+    required this.banners,
+  });
+
+  @override
+  State<GroceryBannerSlider> createState() => _GroceryBannerSliderState();
+}
+
+class _GroceryBannerSliderState extends State<GroceryBannerSlider> {
+  late PageController _pageController;
+  Timer? _timer;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+    _startAutoScroll();
+  }
+
+  void _startAutoScroll() {
+    _timer?.cancel();
+    if (widget.banners.length > 1) {
+      _timer = Timer.periodic(const Duration(seconds: 4), (timer) {
+        if (_pageController.hasClients) {
+          final int nextPage = (_currentPage + 1) % widget.banners.length;
+          _pageController.animateToPage(
+            nextPage,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOutCubic,
+          );
+        }
+      });
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant GroceryBannerSlider oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.banners.length != widget.banners.length) {
+      if (_currentPage >= widget.banners.length) {
+        _currentPage = 0;
+      }
+      _startAutoScroll();
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.banners.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: 180.h,
+          child: PageView.builder(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemCount: widget.banners.length,
+            itemBuilder: (context, idx) {
+              final banner = widget.banners[idx];
+              return GestureDetector(
+                onTap: () {
+                  if (banner.product?.slug != null) {
+                    final product = banner.product!;
+                    context.push(
+                      AppRoutePath.productDetails,
+                      extra: {
+                        'slug': product.slug,
+                        'variantId': product.variants?.firstOrNull?.id,
+                      },
+                    );
+                  }
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 14.w, vertical: 6.h),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18.w),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.08),
+                        blurRadius: 15,
+                        offset: const Offset(0, 6),
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                  child: banner.image != null && banner.image!.isNotEmpty
+                      ? Image.network(
+                          banner.image!,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const ColoredBox(color: Colors.grey),
+                        )
+                      : const ColoredBox(color: Colors.grey),
+                ),
+              );
+            },
+          ),
+        ),
+        if (widget.banners.length > 1) ...[
+          SizedBox(height: 6.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(widget.banners.length, (index) {
+              final bool isSelected = index == _currentPage;
+              return GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.easeInOutCubic,
+                  );
+                },
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 250),
+                  margin: EdgeInsets.symmetric(horizontal: 4.w),
+                  width: isSelected ? 8.w : 6.w,
+                  height: isSelected ? 8.w : 6.w,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isSelected
+                        ? const Color(0xFFFC8019) // Orange for selected
+                        : const Color(0xFFD1D5DB), // Grey for unselected
+                  ),
+                ),
+              );
+            }),
+          ),
+          SizedBox(height: 6.h),
+        ],
+      ],
+    );
+  }
+}
